79167e29d2645995702c5f1c739843566a24e509
Aaro <aaro.varjonen@gmail.com>
2021-10-27 23:01:38 +0300

    src-folder added, still a bit clumsy with Git

---
+++ src/fi/tuni/tamk/tiko/varjonenaaro/Main.java
@@ -0,0 +1,12 @@
+package fi.tuni.tamk.tiko.varjonenaaro;
+
+import fi.tuni.tamk.tiko.varjonenaaro.util.Math;
+
+/**
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Math.getRandom(1, 10));
+    }
+}
